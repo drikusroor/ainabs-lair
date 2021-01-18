2461ec2999c55ff83ddbd04f87f05e8d66943996
Drikus Roor <drikusroor@gmail.com>
2021-01-18 21:27:01 +0000

    Add player join/leave features

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ game_state = { "NEW": "NEW", "PLAYING": "PLAYING", "ENDED": "ENDED" }
 
 games = []
 
+class Player:
+
+  def __init__(self, author, name, hp = 20, ap = 20):
+    self.author = author
+    self.max_hp = hp
+    self.hp = hp
+    self.max_ap = ap
+    self.ap = ap
+
+    if (name):
+      self.name = name
+    else:
+      self.name = author.name
+
 class Game:
 
   def __init__(self, guild, channel):
@@ -18,6 +32,33 @@ class Game:
     self.guild = guild
     self.channel = channel
     self.state = game_state["NEW"]
+
+  def get_player(self, author):
+    for i, o in enumerate(self.players):
+      if o.author == author:
+        return self.players[i]
+    return None
+
+  def kick_player(self, player):
+    for i, o in enumerate(self.players):
+      if o == player:
+        del self.players[i]
+        break
+    return
+
+  def get_readable_player_status(self, player):
+    return "{name} {hp}/{max_hp}HP {ap}/{max_ap}AP".format(
+      name=player.name,
+      hp=player.hp,max_hp=player.max_hp,
+      ap=player.ap,
+      max_ap=player.max_ap
+      )
+
+  def get_game_status(self):
+    statuses = []
+    for i, o in enumerate(self.players):
+      statuses.append(self.get_readable_player_status(o))
+    return "\n".join(statuses)
 
 def get_quote():
   response = requests.get("https://zenquotes.io/api/random")
@@ -48,6 +89,12 @@ async def on_message(message):
 
   guild = message.guild
   channel = message.channel
+
+  def get_game():
+    for i, o in enumerate(games):
+      if o.guild.id == guild.id and o.channel.id == channel.id:
+        return games[i]
+    return None
 
   if message.author == client.user:
     return
@@ -117,6 +164,50 @@ async def on_message(message):
         break
     
     msg = "Game has been killed off. Are you happy now? :cry:"
+    await message.channel.send(msg)
+    return
+
+  if cmd.startswith("join game"):
+    game = get_game()
+    name = message.author.name
+
+    try:
+      name = cmd.split("join game ",1)[1]
+    except:
+      print("Player name not supplied")
+
+    if game.get_player(message.author):
+      msg = "You have already joined the game, you silly. :joy:"
+      await message.channel.send(msg)
+      return
+    else:
+      player = Player(message.author, name)
+      game.players.append(player)
+      msg = "Player {name} has joined the game! :fist:".format(name = player.name)
+      await message.channel.send(msg)
+      return
+
+  if cmd.startswith("leave game"):
+    game = get_game()
+    player = game.get_player(message.author)
+
+    if player:
+      msg = "Player {name} has left the game! :door:".format(name = player.name)
+      game.kick_player(player)
+      await message.channel.send(msg)
+      return
+    else:
+      msg = "You are not in the game, you whackadoodledoo!"
+      await message.channel.send(msg)
+      return
+
+  if cmd.startswith("game status"):
+    game = get_game()
+    msg = game.get_game_status()
+
+    if not msg:
+      msg = "Game is fookin' empty mate. Nothing to see here..."
+
     await message.channel.send(msg)
     return
 
