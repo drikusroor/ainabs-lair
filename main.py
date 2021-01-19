1c7e1c77f2cb3f82d92586f5d4357d3470f8d2de
Drikus Roor <drikusroor@gmail.com>
2021-01-19 16:45:44 +0100

    Refactor python code for local use

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
-import os
 import requests
 import json
 import random
 from random import randrange
-from replit import db
+from decouple import config
+from modules.insult_generator import insult_generator
+
+TOKEN = config('TOKEN')
 
 client = discord.Client()
 
@@ -52,8 +54,6 @@ class Player:
         )
       await ctx.channel.send(msg)
       return
-
-    
 
 class Enemy:
   def __init__(self, game, name, hp = 20, ap = 20):
@@ -162,18 +162,10 @@ def get_quote():
   return(quote)
 
 def update_encouragements(encouraging_message):
-  if "encouragements" in db.keys():
-    encouragements = db["encouragements"]
-    encouragements.append(encouraging_message)
-    db["encouragements"] = encouragements
-  else:
-    db["encouragements"] = [encouraging_message]
+  raise Exception("Not yet implemented.")
 
 def delete_encouragment(index):
-  encouragements = db["encouragements"]
-  if len(encouragements) > index:
-    del encouragements[index]
-  db["encouragements"] = encouragements
+  raise Exception("Not yet implemented.")
 
 @client.event
 async def on_ready():
@@ -205,39 +197,18 @@ async def on_message(message):
     return
 
   if cmd.startswith('cheer'):
-    if "encouragements" in db.keys():
-      options = db["encouragements"]
-      if len(options) > 0:
-        await message.channel.send(random.choice(options))
-        return
-
     no_cheerz = "No cheerz be availables :cry:"
     await message.channel.send(no_cheerz)
     return
 
   if cmd.startswith("list cheer"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      encouragements_raw = db["encouragements"]
-      for idx, val in enumerate(encouragements_raw):
-        encouragements.append("#{i} - {val} ".format(i = idx, val = val))
-    await message.channel.send('\n'.join(encouragements))
-    return
+    raise Exception("Not yet implemented.")
   
   if cmd.startswith("new cheer"):
-    encouraging_message = cmd.split("new cheer ",1)[1]
-    update_encouragements(encouraging_message)
-    await message.channel.send("New encouraging message added.")
-    return
+    raise Exception("Not yet implemented.")
 
   if cmd.startswith("del cheer"):
-    encouragements = []
-    if "encouragements" in db.keys():
-      index = int(cmd.split("del cheer",1)[1])
-      delete_encouragment(index)
-      encouragements = db["encouragements"]
-    await message.channel.send(encouragements)
-    return
+    raise Exception("Not yet implemented.")
 
   if cmd.startswith("new game"):
     for game in games:
@@ -355,6 +326,6 @@ async def on_message(message):
     await message.channel.send(msg)
     return
   
-  await message.channel.send('Not a valid command, {name}. You moron.'.format(name = message.author.name))
+  await message.channel.send('Not a valid command, {name}. You {insult}.'.format(name = message.author.name, insult = insult_generator.generate_insult()))
 
-client.run(os.getenv('TOKEN'))
+client.run(TOKEN)
